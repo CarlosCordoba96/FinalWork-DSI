c6fcace49593939f6ae48c0063cb581cd50ff4c1
Carlos Córdoba Ruiz <carlos.cordoba@alu.uclm.es>
2019-05-31 15:19:19 +0200

    Terminado

--- Hito1.py
+++ Hito1.py
@@ -25,11 +25,6 @@ import itertools
 import seaborn as sns
 import pandas as pd
 
-
-def balancear(dataframeX,dataframeY):
-    dataframeX['mode_main']=dataframeY
-    print(dataframeX['mode_main'].value_counts())
-    
     
 ##Parametrizar las variables no numéricas
 def male(x):
@@ -46,7 +41,7 @@ def ethnicity(x):
     else:
         return 2
 
-    #CARGAMOS LOS DATOS Y REALIZAMOS UN PREPROCESO EN LOS DATOS
+#CARGAMOS LOS DATOS Y REALIZAMOS UN PREPROCESO EN LOS DATOS
 df = pd.read_csv("nts_data.csv")
 df['male']=df['male'].apply(male)
 df['ethnicity']=df['ethnicity'].apply(ethnicity)
@@ -58,32 +53,20 @@ df['weekend']=df['weekend'].map({'yes':1,'no':0})
 
 print(df['mode_main'].value_counts())
 
-#COJEMOS 100.000 DATOS DE MUESTRA
+#COGEMOS 100.000 DATOS DE MUESTRA
 datos=df.sample(n=100000)
 datos=datos.reset_index(drop=True)
 
 datosy=datos['mode_main']
 datos=datos.drop(columns='mode_main',axis=0)
+
+
+
 #                   HITO 1
+#Se crea el modelo random forest con los parámetros especificados en el artículo, 450 árboles
+#tres variables seleccionadas aleatoriamente para cada uno de los nodos de los árboles
 model=RandomForestClassifier(n_estimators =450,min_samples_split=3,n_jobs=-1)
 
-"""
-from sklearn.model_selection import train_test_split
-
-
-X_train,X_test,y_train,y_test = train_test_split(datos,datosy,test_size=0.5, random_state=0)
-model.fit(X_train,y_train)
-
-from sklearn.metrics import accuracy_score,recall_score,confusion_matrix
-y_pred = model.predict(X_test)
-print(len(X_test))
-print(accuracy_score(y_test,y_pred))
-print(confusion_matrix(y_test,y_pred))
-
-y_pred=cross_val_score(model,datos,datosy,cv=10,n_jobs=-1)
-print(y_pred)
-print(np.mean(y_pred))
-"""
 
 #KFOLD 10 CROSS-VALIDATION
 X_trains=[]
@@ -97,9 +80,11 @@ kf = KFold(n_splits=10)
 for train_index, test_index in kf.split(datos):
     X_train, X_test = datos.iloc[train_index], datos.iloc[test_index]
     y_train, y_test = datosy.iloc[train_index], datosy.iloc[test_index]
-    n=int((X_train.shape[0]*(9/10))/4)
+    n=int((X_train.shape[0]*(9/10))/4)#N para calcular nº de registros de cada tipo
     X_train['mode_main']=y_train
-
+    #se junra la columna que queremos predecir y se filtran los datos por cada tipo
+    #de viaje realizado y se realiza un sampleo de N y finalmente se juntan de nuevo
+    #en un mismo dataset
     sub0=X_train.loc[X_train['mode_main']==0]
     sub0=resample(sub0,n_samples=n)
     
@@ -117,17 +102,17 @@ for train_index, test_index in kf.split(datos):
 
     y_train=X_trained['mode_main']
     X_train=X_trained.drop(columns='mode_main',axis=0)
-
+    #Se guardan los conjuntos de datos para utilizarlos en posteriores hitos
     X_trains.append(X_train)
     X_tests.append(X_test)
     y_trains.append(y_train)
     y_tests.append(y_test)
-    
+    #Se cuadran los datos y se crea el modelo y se calcula precisión y sensibilidad
     model.fit(X_train,y_train)
-    scores.append(model.score(X_test, y_test))
+    scores.append(model.score(X_test, y_test))#la precision
     
     predicted=model.predict(X_test)
-    average_precision = recall_score(y_test, predicted,average='macro')
+    average_precision = recall_score(y_test, predicted,average='macro')#la sensibilidad
     
     sensitivity.append(average_precision)
 
@@ -135,33 +120,39 @@ hito1_acc=np.mean(scores)
 hito1_sensi=np.mean(sensitivity)
 print("Media de los valores: {}".format(np.mean(scores)))
 print("Media de la sensibilidad : {}".format(np.mean(sensitivity)))
-print(model.feature_importances_)
+print(model.feature_importances_)#importancia de las variables en el modelo
 
+
+#                           HITO 2
 columnas=['distance','density','age','male','ethnicity','education','income','cars','license',
           'bicycles','weekend','diversity','green','temp','precip','wind']
 
 
-#                           HITO 2
+plt.figure()
+plt.title("Importancia variables ")
+plt.barh(range(len(model.feature_importances_)), model.feature_importances_,color="r", align="center")
+plt.yticks(range(len(model.feature_importances_)), columnas, rotation='horizontal')
+plt.show()
 
 hito2_scores=[]
 hito2_sens=[]
 
 acc2_scores=[]
 
-for variable in columnas:
-    for i in range(0,10):
+for variable in columnas:#para cada variable
+    for i in range(0,10):#en el 10-fold-cross-validatioon
         XNewTest=X_tests[i].copy()
-        XNewTest[variable]=np.random.permutation(XNewTest[variable])
+        XNewTest[variable]=np.random.permutation(XNewTest[variable])#permutacion
         model.fit(X_trains[i],y_trains[i])
         hito2_scores.append(model.score(XNewTest, y_tests[i]))
         predicted=model.predict(XNewTest)
         average_precision = recall_score(y_tests[i], predicted,average='macro')
         hito2_sens.append(average_precision)
-    print("\tLA VARIABLE <<{}>>".format(variable))
-    print("Media de los valores: {}".format(np.mean(hito2_scores)))
-    print("Media de la sensibilidad : {}".format(np.mean(hito2_sens)))
-    print("Diferencia de la precisión: {}".format(np.mean(hito2_scores)-hito1_acc))
-    print("Diferencia de la sensibilidad: {}\n".format(np.mean(hito2_sens)-hito1_sensi))
+    #print("\tLA VARIABLE <<{}>>".format(variable))
+    #print("Media de los valores: {}".format(np.mean(hito2_scores)))
+    #print("Media de la sensibilidad : {}".format(np.mean(hito2_sens)))
+    #print("Diferencia de la precisión: {}".format(np.mean(hito2_scores)-hito1_acc))
+    #print("Diferencia de la sensibilidad: {}\n".format(np.mean(hito2_sens)-hito1_sensi))
     acc2_scores.append(np.mean(hito2_scores)-hito1_acc)
         
 
@@ -172,46 +163,47 @@ plt.yticks(range(len(acc2_scores)), columnas, rotation='horizontal')
 plt.show()
 
 
-XNewTest['mode_main']=y_tests[9]
-sub0new=XNewTest.loc[XNewTest[XNewTest.mode_main==0].index]
-XNewTest=XNewTest.drop(XNewTest[XNewTest.mode_main==0].index)
-sub0new['distance']=np.random.permutation(sub0new['distance'])
-XNewTest=XNewTest.append(sub0new)
-
 modo_viaje=[0,1,2,3]
 hito2_scores=[]
 hito2_sens=[]
 
 acc_scores=[]
 scores_modo=[]
-for modo in modo_viaje:
-    for variable in columnas:
-        for i in range(0,10):
-            XNewTest=X_tests[i].copy()
-            XNewTest['mode_main']=y_tests[i]
-            sub0new=XNewTest.loc[XNewTest[XNewTest.mode_main==modo].index]
+for modo in modo_viaje:#para cada tipo de viaje
+    for variable in columnas:#para cada variable
+        for i in range(0,10):#en el 10fold-cross-validation
+            XNewTest=X_tests[i].copy()#copiamos los datos
+            XNewTest['mode_main']=y_tests[i]#juntamos todo en el mismo dataset
+            sub0new=XNewTest.loc[XNewTest[XNewTest.mode_main==modo].index]#cogemos solo los registros que correspondan al modo de viaje
             XNewTest=XNewTest.drop(XNewTest[XNewTest.mode_main==modo].index)
-            sub0new[variable]=np.random.permutation(sub0new[variable])
-            XNewTest=XNewTest.append(sub0new)
-            y_tests[i]=XNewTest['mode_main']
-            XNewTest=XNewTest.drop(columns='mode_main',axis=0)
+            sub0new[variable]=np.random.permutation(sub0new[variable])#hacemos la permutacion
+            XNewTest=XNewTest.append(sub0new)#lo juntamos
+            y_tests[i]=XNewTest['mode_main']#actualizamos la y
+            XNewTest=XNewTest.drop(columns='mode_main',axis=0)#eliminamos columnas
             
-            model.fit(X_trains[i],y_trains[i])
+            model.fit(X_trains[i],y_trains[i])#entrenamos
             hito2_scores.append(model.score(XNewTest, y_tests[i]))
             predicted=model.predict(XNewTest)
             average_precision = recall_score(y_tests[i], predicted,average='macro')
             hito2_sens.append(average_precision)
-        print("\t EL MODO DE VIAJE:{}".format(modo) )
-        print("\tLA VARIABLE <<{}>>".format(variable))
-        print("Media de los valores: {}".format(np.mean(hito2_scores)))
-        print("Media de la sensibilidad : {}".format(np.mean(hito2_sens)))
-        print("Diferencia de la precisión: {}".format(np.mean(hito2_scores)-hito1_acc))
-        print("Diferencia de la sensibilidad: {}\n".format(np.mean(hito2_sens)-hito1_sensi))
-        acc_scores.append(np.mean(np.mean(hito2_sens)-hito1_sensi))
+#        print("\t EL MODO DE VIAJE:{}".format(modo) )
+#        print("\tLA VARIABLE <<{}>>".format(variable))
+#        print("Media de los valores: {}".format(np.mean(hito2_scores)))
+#        print("Media de la sensibilidad : {}".format(np.mean(hito2_sens)))
+#        print("Diferencia de la precisión: {}".format(np.mean(hito2_scores)-hito1_acc))
+#        print("Diferencia de la sensibilidad: {}\n".format(np.mean(hito2_sens)-hito1_sensi))
+#        acc_scores.append(np.mean(np.mean(hito2_sens)-hito1_sensi))
         print(acc_scores)
+    plt.figure()
+    plt.title("Relevancia {} ".format(modo))
+    plt.barh(range(len(model.feature_importances_)), model.feature_importances_,color="r", align="center")
+    plt.yticks(range(len(model.feature_importances_)), columnas, rotation='horizontal')
+    plt.show() 
     scores_modo.append(acc_scores)
     print(scores_modo)
     acc_scores=[]
+
+print(model.feature_importances_)#importancia de las variables en el modelo
     
 for mode in modo_viaje:
     plt.figure()
@@ -220,25 +212,3 @@ for mode in modo_viaje:
     plt.yticks(range(len(scores_modo[mode])), columnas, rotation='horizontal')
     plt.show()
 
-    
-#importances = model.feature_importances_
-#std = np.std([tree.feature_importances_ for tree in model.estimators_],
-#             axis=0)
-#indices = np.argsort(importances)[::-1]
-#
-## Print the feature ranking
-#print("Feature ranking:")
-#
-#for f in range(X_train.shape[1]):
-#    print("%d. feature %d (%f)" % (f + 1, indices[f], importances[indices[f]]))
-#
-#
-## Plot the feature importances of the forest
-#plt.figure()
-#plt.title("Feature importances")
-#plt.bar(range(len(model.feature_importances_)), importances,
-#       color="r", yerr=std[indices], align="center")
-#plt.xticks(range(X_train.shape[1]), list(X_train.columns.values), rotation='vertical')
-#plt.xlim([-1, X_train.shape[1]])
-#plt.show()
-#print(list(X_train.columns.values))
